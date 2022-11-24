03caa9fbce07ba85a7edd1b1ba420a0d4fdda442
josefzbehlik <115144248+josefzbehlik@users.noreply.github.com>
2022-11-24 11:03:40 +0100

    counter

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
+let number = 0;
 
+basic.forever(function () {
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(number += 1)
+    if (number > 10)
+    {
+        basic.showNumber(number -= 10)
+    }
+    })
+
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(number -= 1)
+    if (number < -5) {
+        basic.showNumber(number += 6)
+    }
+
+})
